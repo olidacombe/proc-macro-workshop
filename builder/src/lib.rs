9c3c9070bb35abcf9518daaeebd317fcfa75d50c
Oli Dacombe <odacombe@blockchain.com>
2021-09-08 00:11:28 +0100

    02-types_need_Option<>

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,13 +1,25 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = input.ident;
+    let builder_name = format_ident!("{}Builder", name);
+
+    let fields = match input.data {
+        Data::Struct(ref s) => match s.fields {
+            Fields::Named(ref fields) => fields.named.iter(),
+            _ => unimplemented!(),
+        },
+        _ => unimplemented!(),
+    };
 
     let expanded = quote! {
+        pub struct #builder_name {
+            #(#fields),*
+        }
         impl #name {
             pub fn builder() {}
         }
