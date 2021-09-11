cb2ed3c83ee3c6333db893e31839d080a94165e5
Oli Dacombe <odacombe@blockchain.com>
2021-09-11 23:09:04 +0100

    03 - implement 02 more succinctly

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, parse_quote, Data, DeriveInput, Fields};
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -8,46 +8,22 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let name = input.ident;
     let builder_name = format_ident!("{}Builder", name);
 
-    let builder_fields = match input.data {
-        Data::Struct(ref s) => match s.fields.clone() {
-            Fields::Named(fields) => fields.named.into_pairs().map(|mut pair| {
-                let mut field = pair.value_mut();
-                let ty = &field.ty;
-
-                field.ty = parse_quote! {
-                    Option<#ty>
-                };
-
-                pair
-            }),
-            _ => unimplemented!(),
-        },
-        _ => unimplemented!(),
-    };
-
-    let builder_defaults = match input.data {
+    let fields = match input.data {
         Data::Struct(s) => match s.fields {
-            Fields::Named(fields) => fields.named.into_pairs().map(|mut pair| {
-                pair.value_mut().ty = parse_quote! {
-                    None
-                };
-                pair
-            }),
+            Fields::Named(fields) => fields.named.into_iter().map(|f| (f.ident, f.ty)),
             _ => unimplemented!(),
         },
         _ => unimplemented!(),
     };
+
+    let (field_names, field_types): (Vec<_>, Vec<_>) = fields.unzip();
 
     let expanded = quote! {
         struct #builder_name {
-            #(#builder_fields)*
+            #(#field_names: Option<#field_types>),*
         }
         impl #name {
-            pub fn builder() -> #builder_name {
-                #builder_name {
-                    #(#builder_defaults)*
-                }
-            }
+            pub fn builder() {}
         }
     };
 
