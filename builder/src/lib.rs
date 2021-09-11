3bec2930a021dd2775dd979f6800cd8b38ac760d
Oli Dacombe <odacombe@blockchain.com>
2021-09-11 16:24:52 +0100

    02

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,16 +1,25 @@
-use proc_macro::TokenStream;
+use proc_macro;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, Data, DeriveInput, Fields};
+use syn::{parse_macro_input, parse_quote, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = input.ident;
     let builder_name = format_ident!("{}Builder", name);
 
     let fields = match input.data {
-        Data::Struct(ref s) => match s.fields {
-            Fields::Named(ref fields) => fields.named.iter(),
+        Data::Struct(s) => match s.fields {
+            Fields::Named(fields) => fields.named.into_pairs().map(|mut pair| {
+                let mut field = pair.value_mut();
+                let ty = &field.ty;
+
+                field.ty = parse_quote! {
+                    Option<#ty>
+                };
+
+                pair
+            }),
             _ => unimplemented!(),
         },
         _ => unimplemented!(),
@@ -18,12 +27,12 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let expanded = quote! {
         pub struct #builder_name {
-            #(#fields),*
+            #(#fields)*
         }
         impl #name {
             pub fn builder() {}
         }
     };
 
-    TokenStream::from(expanded)
+    proc_macro::TokenStream::from(expanded)
 }
