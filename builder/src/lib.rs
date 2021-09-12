59201b692737b450ded53d64c18b445aeb26eaac
Oli Dacombe <odacombe@blockchain.com>
2021-09-12 21:53:39 +0100

    06

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -5,11 +5,17 @@ use syn::{parse_macro_input, Data, DeriveInput, Fields};
 
 // TODO also take type and return empty ident if type
 // is optional itself?
-fn required_option(name: &syn::Ident) -> TokenStream {
+fn required_option_getter(name: &syn::Ident) -> TokenStream {
     let error_msg = format!("Required parameter `{}` not specified", name);
     quote! {
-        #name.take().ok_or(#error_msg)
+        #name.take().ok_or(#error_msg)?
     }
+}
+
+struct BuilderField {
+    ident: syn::Ident,
+    ty: syn::Type,
+    field_getter: proc_macro2::TokenStream,
 }
 
 #[proc_macro_derive(Builder)]
@@ -18,18 +24,41 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let name = input.ident;
     let builder_name = format_ident!("{}Builder", name);
 
-    let fields = match input.data {
+    let fields: Vec<BuilderField> = match input.data {
         Data::Struct(s) => match s.fields {
-            Fields::Named(fields) => fields.named.into_iter().map(|f| (f.ident, f.ty)),
+            Fields::Named(fields) => fields.named.into_iter().map(|f| (f.ident.unwrap(), f.ty)),
             _ => unimplemented!(),
         },
         _ => unimplemented!(),
-    };
+    }
+    .map(|(ident, ty)| {
+        if let syn::Type::Path(ref p) = ty {
+            if let Some(ref seg) = p.path.segments.first() {
+                if seg.ident == "Option" {
+                    if let syn::PathArguments::AngleBracketed(ref args) = &seg.arguments {
+                        if let Some(syn::GenericArgument::Type(ty)) = args.args.first() {
+                            return BuilderField {
+                                field_getter: quote! { #ident.take() },
+                                ident,
+                                ty: ty.clone(),
+                            };
+                        }
+                    }
+                }
+            }
+        }
+        BuilderField {
+            field_getter: required_option_getter(&ident),
+            ident,
+            ty,
+        }
+    })
+    .collect();
 
-    let (field_name, field_type): (Vec<_>, Vec<_>) = fields.unzip();
-    let field_name_required = field_name
-        .iter()
-        .map(|ref n| required_option(&n.as_ref().unwrap()));
+    let field_name: Vec<&syn::Ident> = fields.iter().map(|f| &f.ident).collect();
+    let field_inner_type: Vec<&syn::Type> = fields.iter().map(|f| &f.ty).collect();
+    let field_name_required: Vec<&proc_macro2::TokenStream> =
+        fields.iter().map(|f| &f.field_getter).collect();
 
     let expanded = quote! {
         impl #name {
@@ -40,16 +69,16 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
             }
         }
         pub struct #builder_name {
-            #(#field_name: Option<#field_type>),*
+            #(#field_name: Option<#field_inner_type>),*
         }
         impl #builder_name {
-            #(pub fn #field_name(&mut self, #field_name: #field_type) -> &mut Self {
+            #(pub fn #field_name(&mut self, #field_name: #field_inner_type) -> &mut Self {
                 self.#field_name = Some(#field_name);
                 self
             })*
             pub fn build(&mut self) -> Result<#name, Box<dyn std::error::Error>> {
                 Ok(#name {
-                    #(#field_name: self.#field_name_required?),*
+                    #(#field_name: self.#field_name_required),*
                 })
             }
         }
